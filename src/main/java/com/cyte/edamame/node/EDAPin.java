f9177a1f21ab17d81684e289fe499fa58bd4346c
SVARUN <SVARUN>
2024-01-23 11:57:24 -0800

    - added skeleton EDA classes

---
+++ src/main/java/com/cyte/edamame/node/EDAPin.java
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) 2024. Jeff Wiegley
+ * This work is distributed and licensed under the AGPLv3 license.
+ * Terms and conditions of the AGPLv3 are documented
+ * in the LICENSE.TXT file included in the EDAmame sources.
+ */
+
+package com.cyte.edamame.node;
+
+public class EDAPin extends EDAShape implements EDANodeActions
+{
+
+
+    public void Render()
+    {
+
+    }
+
+    public void Move()
+    {
+
+    }
+}
---
+++ src/main/java/com/cyte/edamame/node/EDAShape.java
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2024. Jeff Wiegley
+ * This work is distributed and licensed under the AGPLv3 license.
+ * Terms and conditions of the AGPLv3 are documented
+ * in the LICENSE.TXT file included in the EDAmame sources.
+ */
+
+package com.cyte.edamame.node;
+
+public class EDAShape
+{
+}
